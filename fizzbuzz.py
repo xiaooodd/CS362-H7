ff8dd8c998b1bbc23e0fa6fcd253de484726248a
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-06-01 13:55:41 +0800

    Implemented fizzbuzz test3
    wrote test3 for fizzbuzz.py, implemented test3 and work success. If n % 5 == 0 and  n % 3 == 0, return FizzBuzz. So input = 15 should return FizzBuzz.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizz(n):
-    if n % 3 == 0:
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
         return "Fizz"
     elif n % 5 == 0:
         return "Buzz"
--- test_fizz.py
+++ test_fizz.py
@@ -8,5 +8,8 @@ class TestCase(unittest.TestCase):
     def test2(self):
         self.assertEqual(fizzbuzz.fizz(5),"Buzz")
 
+    def test3(self):
+        self.assertEqual(fizzbuzz.fizz(15),"FizzBuzz")
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
