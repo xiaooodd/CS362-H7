0bac7e087c4001451ed1c38d115bb1a6e5a3c6a4
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-06-01 13:50:54 +0800

    Implemented fizzbuzz test2
    wrote second test for fizzbuzz.py, implemented second test and work success. If n % 5 == 0, return Buzz. So input = 5 should return Buzz.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,7 @@
 def fizz(n):
     if n % 3 == 0:
         return "Fizz"
-        
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return n
--- test_fizz.py
+++ test_fizz.py
@@ -4,6 +4,9 @@ import fizzbuzz
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(fizzbuzz.fizz(3),"Fizz")
+    
+    def test2(self):
+        self.assertEqual(fizzbuzz.fizz(5),"Buzz")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
