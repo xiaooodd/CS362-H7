73f5bd50b2f345a4e4491569d28f3fb3858f2ffb
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-06-01 14:27:13 +0800

    wrote test3 for leapyear.py
    If input = 4. it can be evenly divisible by 4 but can not be evenly divisible by 100. It should be return Leap year.

--- test_leapyear.py
+++ test_leapyear.py
@@ -8,6 +8,8 @@ class TestCase(unittest.TestCase):
     def test2(self):
         self.assertEqual(leapyear.is_Leap(100),"Not leap year")
 
+    def test3(self):
+        self.assertEqual(leapyear.is_Leap(4),"Leap year")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
