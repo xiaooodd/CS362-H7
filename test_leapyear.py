c628edf2652eafa7b2636c1640aa943b8a798b6f
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-06-01 14:20:43 +0800

    wrote test2 for leapyear.py
    If input = 100, it should be return Not leap year.

--- test_leapyear.py
+++ test_leapyear.py
@@ -5,6 +5,9 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapyear.is_Leap(400),"Leap year")
 
+    def test2(self):
+        self.assertEqual(leapyear.is_Leap(100),"Not leap year")
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
